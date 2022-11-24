ac1e96a75b8183ad63bc5d6f8ff678ee172beb0d
Lilgibbsy <owen@gibbsbunch.com>
2022-11-24 12:18:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,40 @@ function draw() {
   let numRings = +document.getElementById("numRings").value;
 
   let ladder = [{ x: x, y: y, w: w, h: h }];
+  let rings = [{ x: 0, y: 0, w: 0, h: 0 }];
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 700, 500);
+  ctx.restore();
 
   ctx.beginPath();
   ctx.strokeStyle = "red";
-  ctx.moveTo(ladder.x, ladder.y);
-  ctx.lineTo(ladder.y + ladder.h, ladder.x);
+  ctx.moveTo(ladder[0].x, ladder[0].y);
+  ctx.lineTo(ladder[0].x, ladder[0].y + ladder[0].h);
   ctx.stroke();
+  ctx.restore();
+
   ctx.beginPath();
   ctx.strokeStyle = "white";
-  ctx.moveTo(ladder.x + ladder.w, ladder.y);
-  ctx.lineTo(ladder.y + ladder.h, ladder.x);
+  ctx.moveTo(ladder[0].x + ladder[0].w, ladder[0].y);
+  ctx.lineTo(ladder[0].x + ladder[0].w, ladder[0].y + ladder[0].h);
   ctx.stroke();
-  console.log(ladder.x);
+  ctx.restore();
+  let spaceBetweenRings = h / numRings;
+
+  for (let n = 0; n < numRings; n++) {
+    const ring = { x: x, y: spaceBetweenRings, w: w, h: h };
+    spaceBetweenRings += spaceBetweenRings;
+    rings.push(ring);
+  }
+
+  console.log(ladder[0].x, ladder[0].y, ladder[0].w, ladder[0].h);
+  console.log(rings);
 }
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.moveTo(rings.x, rings.y);
+ctx.lineTo(rings.x + w, rings.y);
+ctx.stroke();
+ctx.restore();
